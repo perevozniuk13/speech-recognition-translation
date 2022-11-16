d5737d11f0a15ade1a3bacea19ff67fa7bd0d071
perevozniuk13 <47914997+perevozniuk13@users.noreply.github.com>
2022-11-16 21:42:17 +0000

    Update getAudio.py

--- getAudio.py
+++ getAudio.py
@@ -1,9 +1,6 @@
 from moviepy.editor import *
-from getVideo import title 
+from getVideo import title
 
 
-#print(type(title))
-#print(title, '.mp4')
-#audioclip = AudioFileClip('{title}.mp4')
-audioclip = AudioFileClip("the foolish rabbit  1 minute  Moral story in English  Short  story  for  kids l.mp4")
+audioclip = AudioFileClip('{title}.mp4')
 audioclip.write_audiofile("audio.wav")
